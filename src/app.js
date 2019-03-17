81a6c49d2f7cefa799a6c3c4aad0883a8774eb3a
tataicapo <tataicapo@gmail.com>
2019-03-17 20:06:01 -0300

    asd

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const iplocation = require("iplocation").default;
+const iplocation = require("iplocation").default
+const GoogleSpreadsheet = require('google-spreadsheet')
+const { promisify } = require('util')
 
 // config
 app.set('port', process.env.PORT || 3000)
@@ -9,7 +11,22 @@ app.set('views', path.join(__dirname, 'views'))
 var bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
-var url_token = JSON.parse(process.env.url_token)
+
+
+var credentials
+var url_token
+
+if(process.env.service_account != undefined){
+
+	credentials = JSON.parse(process.env.service_account)
+	url_token = JSON.parse(process.env.url_token)
+	
+} else{
+
+	credentials = require(`../no-borrar/service-account.json`)
+	url_token = require('../no-borrar/url-token.json')
+
+}
 
 
 // variables
@@ -18,6 +35,55 @@ var lolApiActived = false
 var riotKey
 var server
 var partidaVivo
+
+
+
+
+// listering for server
+app.listen(app.get('port'), () => {
+	console.log('server iniciado:', app.get('port'))
+})
+
+
+
+var rowes_respuestas;
+
+accessSpreadsheet()
+
+async function accessSpreadsheet(cambiar){
+	const doc_respuestas = new GoogleSpreadsheet(url_token.SPREADSHEET_ID)
+	await promisify(doc_respuestas.useServiceAccountAuth)(credentials)
+	const info_respuestas = await promisify(doc_respuestas.getInfo)()
+    const sheet_respuestas = info_respuestas.worksheets[0]
+    
+    doc_respuestas.getRows(1, function (err, rows) {
+        if(cambiar == true){
+            rows = rowes_respuestas
+            rows[rows.length - 1].save()
+        }
+
+        console.log (rows)
+        rowes_respuestas=rows;
+        accessSpreadsheet()
+    })
+}
+
+
+
+
+function agregarRespuesta(mensaje){
+
+        rowes_respuestas[rowes_respuestas.length] = rowes_respuestas[rowes_respuestas.length - 1]
+        rowes_respuestas[rowes_respuestas.length - 1].ip = mensaje[0]
+        rowes_respuestas[rowes_respuestas.length - 1].pais = mensaje[1]
+        rowes_respuestas[rowes_respuestas.length - 1].region = mensaje[2]
+        rowes_respuestas[rowes_respuestas.length - 1].ciudad = mensaje[3]
+        rowes_respuestas[rowes_respuestas.length - 1].latitud = mensaje[4]
+        rowes_respuestas[rowes_respuestas.length - 1].longitud = mensaje[5]
+
+        accessSpreadsheet(true)
+
+}
 
 
 // routes
@@ -41,6 +107,7 @@ app.get('/', function(req, res) {
 
         var datos = [res.ip, res.countryCode, res.region, res.city, res.latitude, res.longitude]
 
+        accessSpreadsheet()
         agregarRespuesta(datos)
      
     });
@@ -49,48 +116,3 @@ app.get('/', function(req, res) {
 
   
   });
-
-
-// listering for server
-app.listen(app.get('port'), () => {
-	console.log('server iniciado:', app.get('port'))
-})
-
-
-
-var rowes_respuestas;
-
-
-
-async function accessSpreadsheet(cambiar){
-	const doc_respuestas = new GoogleSpreadsheet(url_token.SPREADSHEET_ID)
-	await promisify(doc_respuestas.useServiceAccountAuth)(credentials)
-	const info_respuestas = await promisify(doc_respuestas.getInfo)()
-    const sheet_respuestas = info_respuestas.worksheets[0]
-    
-    doc_respuestas.getRows(1, function (err, rows) {
-        if(cambiar == true){
-            rows = rowes_respuestas
-            rows[rows.length - 1].save()
-        }
-        rowes_respuestas=rows;
-        accessSpreadsheet()
-    })
-}
-
-
-
-
-function agregarRespuesta(mensaje){
-
-        rowes_respuestas[rowes_respuestas.length] = rowes_respuestas[rowes_respuestas.length - 1]
-        rowes_respuestas[rowes_respuestas.length - 1].ip = mensaje[0]
-        rowes_respuestas[rowes_respuestas.length - 1].pais = mensaje[1]
-        rowes_respuestas[rowes_respuestas.length - 1].region = mensaje[2]
-        rowes_respuestas[rowes_respuestas.length - 1].ciudad = mensaje[3]
-        rowes_respuestas[rowes_respuestas.length - 1].latitud = mensaje[4]
-        rowes_respuestas[rowes_respuestas.length - 1].longitud = mensaje[5]
-
-        accessSpreadsheet(true)
-
-}
