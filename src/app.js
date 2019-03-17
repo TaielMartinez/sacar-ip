36be340f73d0bcf7d86d98bd065e02bf3b218519
tataicapo <tataicapo@gmail.com>
2019-03-17 18:32:34 -0300

    asd

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const path = require('path')
 // config
 app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
 var bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
