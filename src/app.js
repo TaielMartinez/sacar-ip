0ddf4e146eb035f9b25dde34861205b1b8a84b2a
tataicapo <tataicapo@gmail.com>
2019-03-17 18:59:02 -0300

    asd

--- src/app.js
+++ src/app.js
@@ -21,6 +21,15 @@ var partidaVivo
 // routes
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/views/index.html'));
+
+    var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+     console.log('ip client: '+ip)
+
+
+     console.log('ip npm: '+getClientAddress(req))
   
   });
 
